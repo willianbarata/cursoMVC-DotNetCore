3035ec45898d9e8549d59d6a14a38b9d35392249
Willian Barata <willianbarata@gmail.com>
2021-03-07 14:04:43 -0300

    criacao mvc .net

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> categorias { get; set; }
+
+        public DbSet<Produto> produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost\SQLEXPRESS;Database=Cursomvc;Trusted_Connection=True");
+        }
+    }
+}
---
+++ CursoTeste/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CursoTeste
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
